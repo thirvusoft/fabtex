d61456fed36e4f90004a431b74aa731a8662c9f1
manikandaprabhu123 <manikanda.prabhu@thirvusoft.in>
2022-07-29 09:09:07 +0530

    Chore: Patches File Updated

--- fabtex/utils/accounts/sales_invoice.py
+++ fabtex/utils/accounts/sales_invoice.py
@@ -2,6 +2,5 @@ import frappe
 from frappe.custom.doctype.property_setter.property_setter import make_property_setter
 
 
-def execute():
+def sales_invoice_property_setter():
     make_property_setter("Sales Invoice", "einvoice_section", "hidden", 0, "Check")
-    frappe.db.commit()
